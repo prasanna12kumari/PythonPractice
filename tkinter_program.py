b414ffc96bc3c90650ac0bb2603dd165b920e15a
prasanna12kumari <45712304+prasanna12kumari@users.noreply.github.com>
2019-03-01 15:00:09 +0530

    Add files via upload
    Created simple GUI with buttons using Tkinter module

---
+++ tkinter_program.py
@@ -0,0 +1,56 @@
+# my first program in Tkinter
+
+from tkinter import *
+
+# Login form 
+def user_login():
+    frame.grid_forget()
+    
+    global login_frame
+    login_frame = Frame(tk, bg = 'lightsteelblue')
+    login_frame.grid(padx = 100, pady = 20)
+
+    label = Label(login_frame, text = "Login", font = 'bold',bg='lavender',fg='dodgerblue')
+    label.grid(row = 0,padx = 20, pady=20)
+
+    label1 = Label(login_frame, text = "User Name:" ,bg='lavender',fg='dodgerblue')
+    label1.grid(row = 1,padx=20, pady=10)
+
+    label2 = Label(login_frame, text = "Password:" ,bg='lavender',fg='dodgerblue')
+    label2.grid(row = 3,padx=20, pady = 10)
+    global entry1
+    global entry2
+    entry1 = Entry(login_frame)
+    entry1.grid(row = 2,padx=20, pady = 10)
+
+    entry2 = Entry(login_frame,show = '*')
+    entry2.grid(row = 4, pady=10)
+
+    button = Button(login_frame, text = "Submit" ,bg='lavender',fg='dodgerblue')
+    button.grid(row = 5, pady = 20)
+    mainloop()
+
+global tk
+	
+tk = Tk()
+
+tk.config(bg = 'white')
+tk.title('My Window')
+tk.minsize(400,400) # first argument width;second argument height
+# tk.minsize(300,300)
+
+
+global frame
+
+frame = Frame(tk,bg = 'white',width=100,height=500)
+frame.grid(padx=100,pady=100)
+
+button = Button(frame,text="Login",bg='steelblue',command=user_login)
+button.grid(row=0,pady=20)
+
+
+tk.mainloop()
+
+
+
+
